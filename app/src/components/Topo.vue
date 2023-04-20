65029dcebaa2a8d61b97c287f3cb7473ce04c0d6
Wesley Soares <wesley.soares@monetizze.com.br>
2023-04-20 02:22:45 -0300

    feat: adicionando logo

--- app/src/components/Topo.vue
+++ app/src/components/Topo.vue
@@ -2,7 +2,7 @@
     <v-container
         :class="[this.$route.name == 'login' || this.$route.name == 'cadastro' ? '' : 'd-flex justify-space-between']">
         <router-link :to="'/'" data-cy="link_logo" class="d-flex">
-            <v-img src="~@/assets/img/logo.svg" aspect-ratio="1" contain height="67" width="176" />
+            <v-img src="~@/assets/img/logo.svg" aspect-ratio="1" contain height="48" width="180" />
         </router-link>
         <v-btn :class="[this.$route.name == 'login' || this.$route.name == 'cadastro' ? 'd-none' : '']" class="mx-2" fab
             dark small color="#D9D9D9">
